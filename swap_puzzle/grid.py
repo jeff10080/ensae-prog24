2492014fe9206caaa1c4ddc9d31545b5c45864ae
stand <136857057+jeff10080@users.noreply.github.com>
2024-01-15 10:50:32 +0100

    Update grid.py

--- swap_puzzle/grid.py
+++ swap_puzzle/grid.py
@@ -91,9 +91,9 @@ class Grid():
             List of swaps, each swap being a tuple of two cells (each cell being a tuple of integers). 
             So the format should be [((i1, j1), (i2, j2)), ((i1', j1'), (i2', j2')), ...].
         """
-        
-        for swap_call in range (cell_pair_list):
-            self.swap(swap_call)
+
+        for swap_call in cell_pair_list:
+            self.swap(swap_call[0],swap_call[1])
         
 
     @classmethod
