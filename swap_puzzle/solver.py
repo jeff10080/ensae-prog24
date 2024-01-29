65e5ddb8fb3cb3b38beef53f030cd4f2cb08b1b3
stand <136857057+jeff10080@users.noreply.github.com>
2024-01-29 11:24:27 +0100

    Update solver.py

--- swap_puzzle/solver.py
+++ swap_puzzle/solver.py
@@ -5,34 +5,37 @@ class Solver():
     A solver class, to be implemented.
     """
     def __init__(self,g): # g = grid to solve
-        self.state = g.state[:]
+        self.g = g
+        self.state = g.state()
+        self.m = g.m
+        self.n = g.n
     
     def move_seq(self,i1,i2,j1,j2):
-            swap_h,swap_v = [],[]
-            if j2-j1>0:
-                swp_h = [((i1,y),(i1,y + 1)) for y in range(j1,j2)]
-            if j2-j1<0:
-                swp_h = [((i1,y),(i1,y - 1)) for y in range(j1,j2,-1)]
-            if i2-i1>0:
-                swp_v = [((x,j2),(x + 1,j2)) for x in range(i1,i2)]
-            if i2-i1<0:
-                swp_v = [((x,j2),(x - 1,j2)) for x in range(i1,i2,-1)]
-            swap_seq = swap_h + swap_v
-            return swap_seq
+        swap_h,swap_v = [],[]
+        if j2-j1>0:
+            swp_h = [((i1,y),(i1,y + 1)) for y in range(j1,j2)]
+        if j2-j1<0:
+            swp_h = [((i1,y),(i1,y - 1)) for y in range(j1,j2,-1)]
+        if i2-i1>0:
+            swp_v = [((x,j2),(x + 1,j2)) for x in range(i1,i2)]
+        if i2-i1<0:
+            swp_v = [((x,j2),(x - 1,j2)) for x in range(i1,i2,-1)]
+        swap_seq = swap_h + swap_v
+        return swap_seq
     
     def find(self,i2,j2):
-            a = i2*self.n + j2 +1
-            for i1 in range(self.m):
-                for j1 in range(self.n):
-                    if self.state[i1][j1] == a:
-                        return i1,j1
+        a = i2*self.n + j2 +1
+        for i1 in range(self.m):
+            for j1 in range(self.n):
+                if self.state[i1][j1] == a:
+                    return i1,j1
     
     def fetch(self,i2,j2):
-            if self.state[i2][j2] == i2*self.n + j2 +1:
-                pass
-            i1,j1 = self.find(self,i2,j2)
-            swap_seq = self.move_seq(self,i1,i2,j1,j2)
-            return swap_seq
+        if self.state[i2][j2] == i2*self.n + j2 +1:
+            pass
+        i1,j1 = self.find(i2,j2)
+        swap_seq = self.move_seq(i1,i2,j1,j2)
+        return swap_seq
     
     def get_solution(self):
         """
@@ -43,9 +46,9 @@ class Solver():
         solution = []
         for i in range(self.m):
             for j in range(self.n):
-                swapseq = self.fetch(self,i,j)
+                swapseq = self.fetch(i,j)
                 solution += swapseq
-                self.state.swap_seq(self.state,swapseq)
+                self.swap_seq(swapseq)
         return solution
 
 
