0992ba0a7c39fd1289e6525acfce0f5a50f0f1b7
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-01-29 22:03:13 +0100

    Update test_get_solution.py

--- tests/test part 1/test_get_solution.py
+++ tests/test part 1/test_get_solution.py
@@ -15,8 +15,8 @@ class Test_GetSolutions(unittest.TestCase):
         swap_sol = s.get_solution()
         grid_sol = grid.copy()
         print (swap_sol)
-        grid_sol.swap_seq(swap_sol)
         print(grid_sol)
+        grid_sol.swap_seq(swap_sol)
         self.assertEqual(grid_sol.is_sorted(), True) 
 
 if __name__ == '__main__':
