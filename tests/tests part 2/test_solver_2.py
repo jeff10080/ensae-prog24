1c4b5555d0883099fb7673b43d15b121b79950c0
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-02-05 13:08:38 +0100

    Update test_solver_2.py

--- tests/tests part 2/test_solver_2.py
+++ tests/tests part 2/test_solver_2.py
@@ -8,29 +8,21 @@ from grid import Grid
 
 class TestGraphGrid(unittest.TestCase):
     def test_construct_grid_graph(self):
-        # Create a grid from file or any other method you prefer
-        initial_grid = Grid.grid_from_file("input/grid3.in")
-
-        # Create an instance of the Graph class
-        graph = Graph()
 
         # Call the function to construct the grid graph
-        graph.construct_grid_graph(initial_grid)
 
-        self.assertEqual(graph.nb_nodes, expected_number_of_nodes)
+        graph= Graph()
+        graph.construct_grid_graph(Grid.grid_from_file("input/grid1.in"))
+        # Assert that the number of nodes ( 4!)
+
+        self.assertEqual(graph.nb_nodes, 24)
+        self.assertEqual(graph[])
 
         # Assert that the number of edges in the graph is as expected
         self.assertEqual(graph.nb_edges, expected_number_of_edges)
 
         # Add more assertions based on your testing requirements
 
-    def test_bfs(self):
-        # Create a graph from file or any other method you prefer
-        graph = Graph.graph_from_file("input/graph2.in")
-
-        # Add assertions for the bfs function
-        self.assertEqual(graph.bfs(9, 16), [9, 7, 17, 10, 12, 16])
-        self.assertEqual(graph.bfs(9, 11), None)
 
 if __name__ == '__main__':
     unittest.main()
