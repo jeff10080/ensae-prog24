2729737f4357f8a6ebaab2e70282fcfa302ac461
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-02-11 13:05:59 +0100

    Update test_swap.py

--- tests/test part 1/test_swap.py
+++ tests/test part 1/test_swap.py
@@ -8,8 +8,8 @@ from grid import Grid
 class Test_Swap(unittest.TestCase):
     def test_grid1(self):
         grid = Grid.grid_from_file("input/grid1.in")
-        grid.swap((3,0), (3,1))
         grid2 = grid.copy()
+        grid.swap((3,0), (3,1))
         self.assertEqual(grid.state, [[1, 2], [3, 4], [5, 6], [7, 8]])
         self.assertEqual(grid2.state, [[1, 2], [3, 4], [5, 6], [8, 7]])
         
