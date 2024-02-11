57e5f9cc05e9a397f013f1532a0a0be60188ce95
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-02-11 13:33:09 +0100

    Update test_swap_seq.py

--- tests/test part 1/test_swap_seq.py
+++ tests/test part 1/test_swap_seq.py
@@ -8,11 +8,8 @@ from grid import Grid
 class Test_Swap(unittest.TestCase):
     def test_grid1(self):
         grid = Grid.grid_from_file("input/grid1.in")
-        swaps = [((0, 0), (0, 1)), ((1, 0), (0, 0))]
-         
-        for swap in swaps:
-            grid.swap_seq(swap)
-            print(grid.state)
+        swaps = [((0, 0), (0, 1)), ((0, 0), (1, 0))]
+        grid.swap_seq(swaps)
         self.assertEqual(grid.state, [[3, 1], [2, 4], [5,6],[8,7]])
         
 if __name__ == '__main__':
