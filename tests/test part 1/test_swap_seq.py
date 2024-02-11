cbb2aad281f6d2e5e03ad50efaee35a68fd4da9e
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-02-11 20:03:26 +0100

    Update test_swap_seq.py

--- tests/test part 1/test_swap_seq.py
+++ tests/test part 1/test_swap_seq.py
@@ -1,16 +1,19 @@
 # This will work if ran from the root folder ensae-prog24
-import sys 
+import sys
 sys.path.append("swap_puzzle/")
+from grid import Grid
 
 import unittest 
-from grid import Grid
 
 class Test_Swap(unittest.TestCase):
     def test_grid1(self):
         grid = Grid.grid_from_file("input/grid1.in")
+        print(f"Initial state: {grid.state}")
+        
         swaps = [((0, 0), (0, 1)), ((0, 0), (1, 0))]
         grid.swap_seq(swaps)
-        self.assertEqual(grid.state, [[3, 1], [2, 4], [5,6],[8,7]])
         
+        self.assertEqual(grid.state, [[3, 1], [2, 4], [5, 6], [8, 7]])
+
 if __name__ == '__main__':
     unittest.main()
