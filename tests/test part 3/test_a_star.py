76448a7fd8ddcb290fbf38e13588a497fdc16875
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-03-04 09:38:58 +0100

    Update test_a_star.py

--- tests/test part 3/test_a_star.py
+++ tests/test part 3/test_a_star.py
@@ -1,14 +1,19 @@
 # This will work if ran from the root folder ensae-prog24
 import sys 
+
 sys.path.append("swap_puzzle/")
 
 import unittest 
 from graph import Graph
+from grid import Grid
 
 class Test_bfs(unittest.TestCase):
-    def test_graph2(self):
-        graph = Graph.graph_from_file("input/graph2.in")
-        self.assertEqual(graph.bfs(9,16), [9, 7, 17, 10, 12, 16])
+    def test_a_star(self):
+        grid= Grid.grid_from_file("input/grid1.in")
+        graph = Graph()
+        a = graph.construct_grid_graph_bfs(grid)
+        g_sorted = Grid(grid.m,grid.n)
+        self.assertEqual(graph.a_star(grid,g_sorted),[grid.__hash__(),g_sorted.__hash__()] )
         self.assertEqual(graph.bfs(9,11), None)
 
 if __name__ == '__main__':
