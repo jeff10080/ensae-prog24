20ed11d50eabd0d6bf809eb56dd2e399624575a6
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-03-04 10:47:21 +0100

    Update test_get_solution_a_star.py

--- tests/test part 3/test_get_solution_a_star.py
+++ tests/test part 3/test_get_solution_a_star.py
@@ -9,14 +9,14 @@ from graph import Graph
 
 class Test_GetSolutions(unittest.TestCase):
     def test_grid1(self):
-        grid = Grid.grid_from_file("input/grid0.in")
+        grid = Grid.grid_from_file("input/grid5.in")
         grid_sol = grid.copy()
         s = Solver(grid)
         self.assertEqual(grid.is_sorted(), False)
         swap_sol = s.get_solution_a_star(grid)
         print(swap_sol)
         graph = Graph()
-        graph.construct_grid_graph(grid)
+        graph.construct_grid_graph_a_star(grid)
         print(graph.vertices)
         grid.swap_seq(swap_sol)
         self.assertEqual(grid.is_sorted(), True)
