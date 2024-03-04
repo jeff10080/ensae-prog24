1891fdc21575b37f0a88dc43f05306a3f77e9107
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-03-04 21:16:13 +0100

    Update test_grid_from_file.py

--- tests/test part 1/test_grid_from_file.py
+++ tests/test part 1/test_grid_from_file.py
@@ -8,10 +8,11 @@ from grid import Grid
 class Test_GridLoading(unittest.TestCase):
     def test_grid1(self):
         g = Grid.grid_from_file("input/grid1.in")
+        g.display()
         self.assertEqual(g.m, 4)
         self.assertEqual(g.n, 2)
         self.assertEqual(g.state, [[1, 2], [3, 4], [5, 6], [8, 7]])
-        g.display()
+        
 
 if __name__ == '__main__':
     unittest.main()
