d9ad9b3589445369875b2aad0cb14805a3b5c8be
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-03-07 00:17:37 +0100

    Create test_chose_level.py

---
+++ tests/test game/test_chose_level.py
@@ -0,0 +1,19 @@
+# This will work if ran from the root folder ensae-prog24
+import sys 
+sys.path.append("swap_puzzle/")
+
+import unittest 
+from grid import Grid
+from game import Game 
+
+
+class Test_Display(unittest.TestCase):
+    def test_display(self):
+        g = Game(Grid(4,4))
+        g.choose_level()
+        g.display()
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
