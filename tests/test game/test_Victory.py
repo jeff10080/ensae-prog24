77f9c04f0b2b53acc0bd91f59f7a07c7e958090e
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-03-06 14:07:50 +0100

    Create test_Victory.py

---
+++ tests/test game/test_Victory.py
@@ -0,0 +1,20 @@
+# This will work if ran from the root folder ensae-prog24
+import sys 
+sys.path.append("swap_puzzle/")
+
+import unittest 
+from grid import Grid
+from game import Game 
+
+
+class Test_Display(unittest.TestCase):
+    def test_display(self):
+        g = Game(Grid.grid_from_file("input/grid1.in"))
+        g.Victory()
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
+
