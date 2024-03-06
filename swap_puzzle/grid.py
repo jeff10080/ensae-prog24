f939b3de55d0d75d3fbd70c8975c78bbc3f517c5
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-03-06 14:08:03 +0100

    Update grid.py

--- swap_puzzle/grid.py
+++ swap_puzzle/grid.py
@@ -22,7 +22,7 @@ class Grid():
         Note: lines are numbered 0..m and columns are numbered 0..n.
     """
     
-    def __init__(self, m, n, initial_state=None):
+    def __init__(self, m, n, initial_state=None, barriers =[]):
         """
         Initializes the grid.
 
@@ -43,109 +43,13 @@ class Grid():
         if not initial_state:
             initial_state = [list(range(i * n + 1, (i + 1) * n + 1)) for i in range(m)]
         self.state = initial_state
-        self.selected_cells = []
+        self.barriers = barriers
+        
 
     
 
-    def display(self):
-        pygame.init()
+    
 
-        width = self.n * 100
-        height = self.m * 100
-
-        screen = pygame.display.set_mode((width, height + 100))  # Ajout de l'espace pour le bouton
-
-        clock = pygame.time.Clock()  # Créer une horloge pour gérer la vitesse de la boucle principale
-
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if 0 < event.pos[0] < width and height < event.pos[1] < height + 100:
-                        running = False
-                    clicked_row = event.pos[1] // 100
-                    clicked_col = event.pos[0] // 100
-                    if 0 <= clicked_row < self.m and 0 <= clicked_col < self.n:
-                        clicked_cell = (clicked_row, clicked_col)
-                        if len(self.selected_cells) == 1:
-                            selected_cell = self.selected_cells[0]
-                            if clicked_cell == selected_cell:
-                                self.selected_cells = []
-                            elif self.test_valid_swap(clicked_cell, selected_cell):
-                                self.swap(clicked_cell, selected_cell)
-                                self.selected_cells = []
-                                if self.is_sorted():
-                                    print("YOU WIN")
-                                    running = False
-                            else:
-                                print(f"Invalid swap: {clicked_cell} and {selected_cell}")
-                        else:
-                            self.selected_cells.append(clicked_cell)
-
-            screen.fill((0, 0, 0))  # Effacer l'écran
-            for i in range(self.m):
-                for j in range(self.n):
-                    cell_rect = pygame.Rect(j * 100, i * 100, 100, 100)
-                    pygame.draw.rect(screen, (255, 255, 255), cell_rect)
-                    font = pygame.font.Font(None, 72)
-                    text = font.render(str(self.state[i][j]), True, (0, 0, 0))
-                    text_rect = text.get_rect(center=cell_rect.center)
-                    screen.blit(text, text_rect)
-                    if (i, j) in self.selected_cells:
-                        pygame.draw.rect(screen, (255, 255, 0), cell_rect, 5)
-
-            pygame.draw.rect(screen, (255, 0, 0), (0, height, width, 100))  # Rectangle rouge pour le bouton Quitter
-            font = pygame.font.Font(None, 72)
-            text = font.render("Quitter", True, (255, 255, 255))
-            text_rect = text.get_rect(center=(width // 2, height + 50))
-            screen.blit(text, text_rect)
-            
-
-            pygame.display.flip()  # Mettre à jour l'affichage
-            clock.tick(30)  # Limiter la vitesse de la boucle principale à 30 images par seconde
-            
-        screen.fill((0, 0, 0))  # Effacer l'écran   
-        if self.is_sorted():
-
-            # Créer une surface de l'écran complet
-            
-            font_win = pygame.font.Font(None, 300)
-            text_win = font_win.render("YOU WIN", True, (255, 255, 255))
-            text_width, text_height = text_win.get_width(), text_win.get_height()
-
-            
-            screen_info = pygame.display.Info()
-            screen_width, screen_height = screen_info.current_w, screen_info.current_h
-
-            # Create a new surface with dimensions based on the text size
-            screen = pygame.display.set_mode((text_width, text_height))
-
-            # Center the window on the screen
-            window_rect = screen.get_rect(center=(text_width, text_height))
-            text_rect_win = text_win.get_rect(center=window_rect.center)
-            screen.blit(text_win, text_rect_win)
-
-            pygame.display.flip()
-
-            # Wait for a short moment for the player to see the "YOU WIN" message
-            pygame.time.delay(2000)
-        
-
-        for i in range(self.m):
-            for j in range(self.n):
-                cell_rect = pygame.Rect(j * 100, i * 100, 100, 100)
-                pygame.draw.rect(screen, (255, 255, 255), cell_rect)
-                font = pygame.font.Font(None, 72)
-                text = font.render(str(self.state[i][j]), True, (0, 0, 0))
-                text_rect = text.get_rect(center=cell_rect.center)
-                screen.blit(text, text_rect)
-                if (i, j) in self.selected_cells:
-                    pygame.draw.rect(screen, (255, 255, 0), cell_rect, 5) 
-        
-        pygame.quit()
 
     def __hash__(self):
         state_tuple = tuple(tuple(self.state[i]) for i in range(self.m))
@@ -205,9 +109,11 @@ class Grid():
 
     def test_valid_swap(self,cell1,cell2):
         i1,j1,i2,j2 = cell1[0],cell1[1],cell2[0],cell2[1]
-        return (abs(i1-i2) == 1 and abs(j1-j2) == 0) or (abs(i1-i2) == 0 and abs(j1-j2) == 1)
+        return (abs(i1-i2) == 1 and abs(j1-j2) == 0) or (abs(i1-i2) == 0 and abs(j1-j2) == 1) and (cell1,cell2) not in self.barriers
     
-    #def test_obstacle_swap(self,cell1,cell2,l = []):
+    
+    
+    
 
 
 
