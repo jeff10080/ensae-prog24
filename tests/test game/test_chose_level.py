ddf348e75727f41d0b527b6e6c71796d09d535af
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-03-07 10:28:03 +0100

    Update test_chose_level.py

--- tests/test game/test_chose_level.py
+++ tests/test game/test_chose_level.py
@@ -5,12 +5,18 @@ sys.path.append("swap_puzzle/")
 import unittest 
 from grid import Grid
 from game import Game 
+from solver import Solver
 
 
 class Test_Display(unittest.TestCase):
     def test_display(self):
-        g = Game(Grid(4,4))
+        grid = Grid(4,4)
+        g = Game(grid)
         g.choose_level()
+        grid1 = Grid(g.m,g.n,g.state)
+        s = Solver(grid1)
+        swap_sol = s.get_solution_a_star(grid)
+        print(swap_sol)
         g.display()
         
         
