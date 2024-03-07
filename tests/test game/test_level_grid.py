f7aee9c17d71b47a100c93e272fc5b074e1db3c3
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-03-07 12:55:53 +0100

    Update test_level_grid.py

--- tests/test game/test_level_grid.py
+++ tests/test game/test_level_grid.py
@@ -7,8 +7,8 @@ from game import Game
 
 class Test_level_grid(unittest.TestCase):
     def test_display(self):
-        grid =Grid(5,5).level_grid(10)
-        print(grid.heuristic())
+        grid =Grid(5,5).level_grid(4)
+        #print(grid.heuristic())
         Game(grid).display()
         
 if __name__ == '__main__':
