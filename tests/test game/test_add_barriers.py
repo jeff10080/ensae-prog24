0124a69233361fca4973b8bffd943b42ee63f5a7
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-03-10 23:16:59 +0100

    Create test_add_barriers.py

---
+++ tests/test game/test_add_barriers.py
@@ -0,0 +1,35 @@
+# This will work if ran from the root folder ensae-prog24
+import sys 
+sys.path.append("swap_puzzle/")
+
+import unittest 
+from grid import Grid
+from solver import Solver
+
+class Test_add_barriers(unittest.TestCase):
+    def test_grid1(self):
+        grid = Grid.grid_from_file("input/grid4.in")
+        for _ in range (10):
+            grid.add_barriers()
+            print(grid.barriers) 
+            self.assertEqual(grid.valid_barriers(), True)
+
+        s= Solver(grid)
+        print(grid.barriers)
+        swap_sol= s.get_solution_a_star(grid)
+        
+        for swap in swap_sol:
+            print(swap)
+            self.assertEqual(grid.test_valid_swap(swap[0],swap[1]), True)
+        
+       
+        
+        #barrière complète
+        
+        
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
+
