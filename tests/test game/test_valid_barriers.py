84c98442869a78dd39d15830215828b2bb8da4a0
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-03-10 17:48:04 +0100

    Update test_valid_barriers.py

--- tests/test game/test_valid_barriers.py
+++ tests/test game/test_valid_barriers.py
@@ -9,10 +9,11 @@ class Test_GetSolutions(unittest.TestCase):
     def test_grid1(self):
         grid = Grid.grid_from_file("input/grid4.in")
         self.assertEqual(grid.valid_barriers(), True)
-        grid.barriers = [((0,0),(0,1)),((1,0),(1,1)),((2,0),(2,1)), ((1,1),(1,2)),((2,1),(2,2)),((3,1),(3,2))]
+        grid.barriers = set([((0,0),(0,1)),((1,0),(1,1)),((2,0),(2,1)), ((1,1),(1,2)),((2,1),(2,2)),((3,1),(3,2))])
         self.assertEqual(grid.valid_barriers(), True)
-        grid.barriers.append(((3,0),(3,1)))
-        self.assertEqual(grid.valid_barriers(), None)
+        grid.barriers.add(((3,0),(3,1)))
+        #barrière complète
+        self.assertNotEqual(grid.valid_barriers(), True)
         
         
 
