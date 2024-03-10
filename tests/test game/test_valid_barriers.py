83af24139a9dc67eb4f4980f4d333e59aa78c61c
avnerelbaz3500 <156650193+avnerelbaz3500@users.noreply.github.com>
2024-03-10 16:25:14 +0100

    Create test_valid_barriers.py

---
+++ tests/test game/test_valid_barriers.py
@@ -0,0 +1,22 @@
+# This will work if ran from the root folder ensae-prog24
+import sys 
+sys.path.append("swap_puzzle/")
+
+import unittest 
+from grid import Grid
+
+class Test_GetSolutions(unittest.TestCase):
+    def test_grid1(self):
+        grid = Grid.grid_from_file("input/grid4.in")
+        self.assertEqual(grid.valid_barriers(), True)
+        grid.barriers = [((0,0),(0,1)),((1,0),(1,1)),((2,0),(2,1)), ((1,1),(1,2)),((2,1),(2,2)),((3,1),(3,2))]
+        self.assertEqual(grid.valid_barriers(), True)
+        grid.barriers.append(((3,0),(3,1)))
+        self.assertEqual(grid.valid_barriers(), None)
+        
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
+
